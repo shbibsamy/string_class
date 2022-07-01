c0dabd580f3aa22efb51653f38c126c77e038d41
shbibsamy <shbibsamy@gmail.com>
2022-07-01 16:27:27 +0200

    Initial Commit

---
+++ exercice.md
@@ -0,0 +1,24 @@
+---
+
+Author: Alain ORLUK / ID-Formation  
+Formation : Développeur Web & Web mobile  
+Lieu: Strasbourg
+Date : 30/06/2022  
+
+---
+# **Cr&eacute;ation d'une classe inspir&eacute;e de la syntaxe Javascript**
+
+Vous avez j'espère encore en mémoire les foncrtions natives que nous avons utilisées en JavaScript et notamment celles qui permettaient d'effectuer des opérations sur les chaînes de caractères.  
+Car comme je vous l'avais déjà dit, il est possible de développer en JavaScript en utilisant la POO.
+
+Mais revenons aux fonctions natives de JavaScript.  
+
+Ces fonctions, telles que indexOf() ou startsWith() sont en fait des méthodes implémentées par la classe String.  
+Voici un lien vers la doc JavaScript de cette classe :  
+[Doc JavaScript de la classe String](https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/String)  
+
+Je vous propose un exercice qui va je pense vous aider à mieux comprendre l'utilité de la POO en général.  
+
+Je vous demande de créer en PHP une classe "String" qui implémentera au moins 5 méthodes inspirées de celles qui existent en JavaScript.  
+
+Il vous faudra ensuite écrire un programme intéractif qui permettra d'expérimenter toutes les méthodes que vous aurez créées.
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>StringClass</title>
+</head>
+<body>
+    <h1>Testing the functions of a new class</h1>
+    <form action='index.php' method='POST'>
+        <h2>Quick form</h2>
+        <fieldset>
+            <label>
+                <span>
+                    Input a string to test the functionality of my new class.
+                </span>
+                <input type='text' name='my-string'>
+            </label>
+            <input type='submit' name='submit'>
+        </fieldset>
+    </form>
+    <div class='result'>
+        <div class='function'>
+            <h2>Function 1:</h2>
+            <span></span>
+        </div>
+        <div class='function'>
+            <h2>Function 2:</h2>
+            <span></span>
+        </div>
+        <div class='function'>
+            <h2>Function 3:</h2>
+            <span></span>
+        </div>
+        <div class='function'>
+            <h2>Function 4:</h2>
+            <span></span>
+        </div>
+        <div class='function'>
+            <h2>Function 5:</h2>
+            <span></span>
+        </div>
+    </div>
+</body>
+</html>
